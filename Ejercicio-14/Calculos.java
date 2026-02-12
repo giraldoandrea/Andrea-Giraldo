4ab5aa17a7a148bddd6c56e3644dc90db16c4c97
giraldoandrea <angiraldore@unal.edu.co>
2026-02-12 11:03:03 -0500

    Create Calculos.java

---
+++ Ejercicio-14/Calculos.java
@@ -0,0 +1,10 @@
+public class Calculos {
+    public static double calcular_cuadrado(double numero) {
+   return Math.pow(numero,2);
+}
+
+    public static double calcular_cubo(double numero) {
+       return Math.pow(numero,3);
+    }       
+    
+}
